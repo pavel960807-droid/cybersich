214ce27eb92dbc9aa62db4a4a2a4b23dbad5c6f5
pavel960807-droid <pavel960807@gmail.com>
2026-02-08 09:29:24 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,9 @@
+// js/config.js
+const CONFIG = {
+    VERSION: "5.0.0",
+    PRIMARY_COLOR: "#00f2ff",
+    SECONDARY_COLOR: "#fff200",
+    GRID_SIZE: 100,
+    MINING_RATE: 0.005,
+    MAX_NFTS: 50
+};
